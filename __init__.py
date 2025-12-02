04b2c7c9e7c6836bb45fd692151da17f988bf18f
safae-2003 <130058071+safae-2003@users.noreply.github.com>
2025-12-02 11:46:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,34 +1,32 @@
 from flask import Flask, render_template, jsonify
-from urllib.request import urlopen
 import json
 from datetime import datetime
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
-# Route principale (page d'accueil)
+# Route accueil
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-# Exercice 2 : Route contact
+# Route contact (Exercice 2)
 @app.route('/contact/')
-def contact_page():
+def contact():
     return "<h2>Ma page de contact</h2>"
 
-# Exercice 3 : API météo Tawarano
+# Route météo (Exercice 3)
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
-
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin → °C
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion Kelvin -> °C
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-# Exécution de l'application
 if __name__ == "__main__":
     app.run(debug=True)
