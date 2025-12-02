d6acce4f542b78f1320a3256420de1661d2fed63
safae-2003 <130058071+safae-2003@users.noreply.github.com>
2025-12-02 11:57:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, render_template_string, jsonify
+from flask import render_template
 import json
 from datetime import datetime
 from urllib.request import urlopen
+import sqlite3
 
 app = Flask(__name__)
 
@@ -11,11 +13,11 @@ def hello_world():
     return render_template('hello.html')
 
 # Route contact (Exercice 2)
-@app.route('/contact/')
-def contact():
+@app.route("/contact/")
+def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
-# Route météo (Exercice 3)
+# Route Tawarano (Exercice 3)
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -24,14 +26,19 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin -> °C
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Kelvin -> °C
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-# Route rapport (Exercice 3 Bis)
+# Route rapport graphique (Exercice 3 Bis)
 @app.route('/rapport/')
 def mongraphique():
-    return render_template('graphique.html')
+    return render_template("graphique.html")
+
+# Route histogramme (Exercice 4)
+@app.route('/histogramme/')
+def histogramme():
+    return render_template("histogramme.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
