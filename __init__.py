4201d557638c51c3a9bafdf427594292f8b49e98
safae-2003 <130058071+safae-2003@users.noreply.github.com>
2025-12-02 11:49:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,9 +24,14 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion Kelvin -> °C
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin -> °C
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+# Route rapport (Exercice 3 Bis)
+@app.route('/rapport/')
+def mongraphique():
+    return render_template('graphique.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
